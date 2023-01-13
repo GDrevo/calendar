2341e3ec72c949758fa92fe318ee812429633e09
guillaume drevo <guillaume.drevo@gmail.com>
2023-01-13 18:42:10 +0100

    simple calendar added

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -4,6 +4,15 @@ class MeetingsController < ApplicationController
   end
 
   def create
+    @meeting = Meeting.new(meeting_params)
+    @meeting.user = current_user
+    @meeting.save
+    redirect_to root_path
+  end
 
+  private
+
+  def meeting_params
+    params.require(:meeting).permit(:start_time, :end_time)
   end
 end
