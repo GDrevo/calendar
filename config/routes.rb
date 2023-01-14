ae3b754487e1d2c845fba34797c7943b191b49cb
guillaume drevo <guillaume.drevo@gmail.com>
2023-01-14 18:58:22 +0100

    added update and destroy methods for meetings

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :meetings, only: %i[new create]
+  resources :meetings, only: %i[new create edit update destroy]
 end
